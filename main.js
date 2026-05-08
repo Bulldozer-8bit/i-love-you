5eb2db7c04a3cb9c7fd96294def7efb4682895d4
Bulldozer-8bit <3101805238@qq.com>
2026-05-08 10:29:17 +0800

    lets go

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ i.attach(server);
 
 //defaults: port 80 & 127.0.0.1. you can override this with "port=1234 node X.js" on CLI
 var port = process.env.port !== undefined ? process.env.port : 80;
-var ip = "127.0.0.1";
+var ip = "0.0.0.0";
 
 //RedHat Openshift stuff. ignore or delete if you are not hosting on Openshift.
 port = process.env.OPENSHIFT_NODEJS_PORT !== undefined ? process.env.OPENSHIFT_NODEJS_PORT : port;
